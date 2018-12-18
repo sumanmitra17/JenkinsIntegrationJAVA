2ada8c314473240a10420bb5cc4bcbd7275b83fa
sumanmitra17 <suman.mitra@indusnet.co.in>
2018-12-18 13:28:44 +0530

    First commit

---
+++ FullIntegration/src/test/java/IntePackage/IntegrationJenk.java
@@ -0,0 +1,27 @@
+package IntePackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class IntegrationJenk {
+
+	static WebDriver dr;
+	@Test(priority=0)
+	public static void testHello()
+	{
+		System.out.println("Welcome to the World of Maven.........");
+		System.setProperty("webdriver.chrome.driver", "E:\\Suman\\Softwares\\Cell\\Drivers\\Chrome\\chromedriver.exe");
+		dr = new ChromeDriver();
+		dr.get("https://www.google.com");
+		System.out.println("The page title is : " + dr.getTitle()+" really got it man........????");
+	}
+	
+	@Test(priority=1)
+	public static void failingTest() throws Exception
+	{
+		dr.findElement(By.name("sd")).click();
+	}
+	
+}
